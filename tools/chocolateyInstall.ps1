e009f19e6c72ea5c4cf69f3948624e4d70b28994
Ken Dale <ken@kendaleiv.com>
2015-10-02 15:36:00 -0400

    Update to VeraCrypt 1.15, add note about non-silent install and uninstall

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = "veracrypt"
 $fileType = "exe"
 $silentArgs = "" # Silent installation discussion: https://veracrypt.codeplex.com/discussions/579539
-$url = "https://veracrypt.codeplex.com/downloads/get/1372345"
+$url = "https://veracrypt.codeplex.com/downloads/get/1468023"
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url

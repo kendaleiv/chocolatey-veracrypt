035c31c47d1e03f913c3e218038f595a603b10fb
Chandler <chandler.newby@gmail.com>
2015-12-11 11:53:24 -0700

    Added AutoHotKey scripts for install/uninstall

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,31 @@
-﻿$packageName = "veracrypt"
-$fileType = "exe"
-$silentArgs = "" # Silent installation discussion: https://veracrypt.codeplex.com/discussions/579539
-$url = "https://veracrypt.codeplex.com/downloads/get/1468023"
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+$packageName= 'veracrypt'
+$toolsDir   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$url        = 'https://veracrypt.codeplex.com/downloads/get/1468023'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url           = $url
+
+  silentArgs = "" # Silent installation discussion: https://veracrypt.codeplex.com/discussions/579539
+
+  softwareName  = 'VeraCrypt*'
+  checksum      = 'aafacca9a600af5b8d66387718c984b8655905f72370bbd772baf90e57e85b7e'
+  checksumType  = 'sha256'
+}
+
+#Thanks to dtgm and the GitHub package for ideas.
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $toolsDir "veracryptInstall.ahk"
+$ahkProc = Start-Process -FilePath $ahkExe `
+                         -ArgumentList $ahkFile `
+                         -PassThru
+
+$ahkId = $ahkProc.Id
+Write-Debug "$ahkExe start time:`t$($ahkProc.StartTime.ToShortTimeString())"
+Write-Debug "Process ID:`t$ahkId"
+
+Install-ChocolateyPackage @packageArgs
+

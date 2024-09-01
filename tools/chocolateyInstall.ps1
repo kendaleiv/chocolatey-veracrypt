944fb839470cb4429741b0b0a307570bb250d61f
Ken Dale <1012917+kendaleiv@users.noreply.github.com>
2024-09-01 10:58:28 -0400

    1.26.14

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://launchpad.net/veracrypt/trunk/1.26.7/+download/VeraCrypt_Setup_x64_1.26.7.msi'
+$url64      = 'https://launchpad.net/veracrypt/trunk/1.26.14/+download/VeraCrypt_Setup_x64_1.26.14.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   fileType      = 'MSI'
   url64bit      = $url64
   softwareName  = 'VeraCrypt*'
-  checksum64    = 'c802dcdda02e9a7a3f52645bf90285d3055c76038dc760131192cc23327ca8f8'
+  checksum64    = '9A29C7B3B911BBC50BC7F41F93C7659CE70599FD19A78ACA002BD6E4A8D3E9A4'
   checksumType64= 'sha256'
   silentArgs    = "/qn /norestart ACCEPTLICENSE=YES"
   validExitCodes= @(0)
